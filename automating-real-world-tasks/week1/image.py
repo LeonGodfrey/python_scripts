4e04b15b7ad344d3c69d355c97341600ade5d3d5
ssegawa <ssegodfrey171@gmail.com>
2022-10-02 12:37:33 +0300

    lab 2 for module 6 passed

---
+++ automating-real-world-tasks/week1/image.py
@@ -0,0 +1,13 @@
+from PIL import Image
+im = Image.open("example.jpg")
+new_im = im.resize((640,480))
+new_im.save("example_resized.jpg") #resize img
+
+#rotate
+new_im = im.rotate(90)
+new_im.save("example_rotated.jpg")
+
+#combine ops
+im.rotate(180).resize((640,480)).save("flipped_and_resized.jpg")
+
+
